3d749d20a356d1aed64504c367a9e6288e1b1deb
lalnatalia <44715525+lalnatalia@users.noreply.github.com>
2018-11-10 00:45:07 -0500

    Add files via upload

--- src/NewEntry/NewEntryController.java
+++ src/NewEntry/NewEntryController.java
@@ -4,6 +4,9 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import database.dbConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,9 +20,11 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import sample.dbStatus;
 
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
@@ -28,12 +33,17 @@ public class NewEntryController implements Initializable{
     @FXML private JFXButton addentry, clear, cancel;
     @FXML private JFXTextField username, urladdress;
     @FXML private JFXPasswordField password;
-    //needs to use enum  @FXML private JFXComboBox<>;
-    public void initialize(URL url, ResourceBundle rb)
-    {
+    @FXML private JFXComboBox<groupOption> groups;
 
+    dbStatus dataBase= new dbStatus();
+    public void initialize(URL url, ResourceBundle rb) {
+        if (dataBase.isDatabaseConnected()) {
+            System.out.print("Database connected succesfully");
+        } else {
+            System.out.print("Problem with daatabase connection");
+        }
+        this.groups.setItems(FXCollections.observableArrayList(groupOption.values()));
     }
-
 
 
     @FXML
@@ -42,6 +52,8 @@ public class NewEntryController implements Initializable{
         username.setText("");
         urladdress.setText("");
         password.setText("");
+
+
     }
 
     @FXML
