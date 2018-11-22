1f896d1bb74085c5bba2e953758f33e81954451f
jayeon <jayeon@yandex.com>
2018-11-22 15:39:49 -0500

    Update Watchtower.java
    Debugging

--- Watchtower.java
+++ Watchtower.java
@@ -4,6 +4,9 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
+import javax.net.ssl.HttpsURLConnection;
+import org.apache.commons.validator.routines.*;
+
 /**
  * @author jayeon
  * The Watchtower class models the methods found in the Watchtower page
@@ -54,28 +57,40 @@ public class Watchtower {
 	 */
 	private URL standardiseWebsite(String website) throws IOException, MalformedURLException {	
 		//check whether the website has a protocol
-		//adds if it is missing
-		if ((website.startsWith("https://") || website.startsWith("https://")) == false) {
+		//adds protocol if it is missing
+		if (!(website.startsWith("https://") && website.startsWith("http://"))) {
 			website = "http://" + website;
 		}
 		
-		URL site = new URL(website);		
-		HttpURLConnection connect = (HttpURLConnection) site.openConnection();
-		int code;
+		URL site = new URL(website);
 		
-		code = connect.getResponseCode();
-		if (code == HttpURLConnection.HTTP_OK) {
-			return site;
-		} else if ((code == HttpURLConnection.HTTP_MOVED_TEMP
-				|| code == HttpURLConnection.HTTP_MOVED_PERM
-				|| code == HttpURLConnection.HTTP_SEE_OTHER)) {
-			site = new URL(connect.getHeaderField("Location")); //looks for redirects
-			connect = (HttpURLConnection) site.openConnection();
-			code = connect.getResponseCode();
-			if (code == HttpURLConnection.HTTP_OK) {
-				return site;
+		//checks whether the url is valid
+	    UrlValidator urlValidator = new UrlValidator();
+	    if (!urlValidator.isValid(website)) {
+	    	return site;
+	    }
+		
+	    //updates site with possible redirects
+		if (site.getProtocol().equals("http")) {
+			HttpURLConnection connect = (HttpURLConnection) site.openConnection();
+			int code = connect.getResponseCode();
+			if ((code == HttpURLConnection.HTTP_MOVED_TEMP
+					|| code == HttpURLConnection.HTTP_MOVED_PERM
+					|| code == HttpURLConnection.HTTP_SEE_OTHER)) {
+				site = new URL(connect.getHeaderField("Location"));
 			}
-		}
+	    }
+	    
+	    if (site.getProtocol().equals("https")) {
+	    	HttpsURLConnection connects = (HttpsURLConnection) site.openConnection();
+			int code2 = connects.getResponseCode();
+			if ((code2 == HttpsURLConnection.HTTP_MOVED_TEMP
+					|| code2 == HttpsURLConnection.HTTP_MOVED_PERM
+					|| code2 == HttpsURLConnection.HTTP_SEE_OTHER)) {
+				site = new URL(connects.getHeaderField("Location"));
+			}
+	    }
+	    
 		return site;
 	}
 	
@@ -90,11 +105,16 @@ public class Watchtower {
 	 * @throws MalformedURLException
 	 */
 	public boolean checkUnsecure(String website) throws IOException, MalformedURLException {
-		URL site = standardiseWebsite(website);
+	    URL site = standardiseWebsite(website);
+		
+		UrlValidator urlValidator = new UrlValidator();
+	    if (!urlValidator.isValid(site.toString())) {
+	    	return true;
+	    }
+		
 		if (site.getProtocol().equals("http")) {
 			return false;
-		} else {
-			return true;
 		}
+		return true;
 	}
 }
